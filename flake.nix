a9d3b5423fb60f2f21dc66f632d4c90d93d655cb
Peter Alexander <makeroftools@creativedevelopment.systems>
2021-09-08 10:57:04 -0400

    yarn2nix flakify wip

--- flake.nix
+++ flake.nix
@@ -2,17 +2,29 @@
   description = "use GraphQL query language to access data in remote APIs that don't run GraphQL";
  
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.graphql-src.url = "github:Urigo/graphql-mesh";
+  inputs.graphql-src.flake = false;
 
-  outputs = { self, nixpkgs, flake-utils }: 
+  outputs = { self, nixpkgs, flake-utils, graphql-src }: 
     (flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; }; 
-        gql = import ./default.nix { inherit pkgs; };
+        pkgs = import nixpkgs { inherit system; };
+        name = "graphql-mesh-monorepo";
       in 
         {
-          packages.graphql-mesh-monorepo = gql.package;
-          defaultPackage = gql.package;
-          devShell = gql.shell;
+          packages."${name}" = pkgs.mkYarnPackage {
+            name = "graphql-mesh-monorepo";
+            src = graphql-src;
+            packageJSON = ./package.json;
+            yarnLock = ./yarn.lock;
+            yarnNix = ./yarn.nix;
+          };
+          defaultPackage = self.packages.graphql-mesh-monorepo;
+          devShell = pkgs.mkShell {
+            buildInputs = [
+              self.defaultPackage
+            ];
+          };
         }
     ));
 }
